b1b5b2427ba67b7fbe86d3b4132b45099125cbe4
Isabella <sorianoisabella28@gmail.com>
2023-11-29 19:24:33 -0500

    implementar product_service

---
+++ lib/services/product_service.dart
@@ -0,0 +1,33 @@
+
+import 'dart:convert';
+import 'dart:io';                               //importar .io
+import 'package:ecommerce_app/models/product.dart';
+import 'package:http/http.dart' as http;
+
+class ProductService{
+
+  //https://plain-marbled-muskox.glitch.me/shoes
+
+  final String _baseUrl="https://plain-marbled-muskox.glitch.me/users?";
+
+  //devuelve en algun momento una lista
+  Future<List> getAll() async{
+
+    http.Response response=await http.get(Uri.parse(_baseUrl));
+
+    //si la respuesta devuelve algo
+    if(response.statusCode==HttpStatus.ok){
+
+      //convertir la respuesta en un arreglo
+      final List jsonResponse=json.decode(response.body);
+
+      //retorna una lista de todos los objetos
+      return jsonResponse.map((e)=> Product.fromJson(e)).toList();
+    }
+    //sino retorna un arreglo vacio
+    return [];
+  }
+}
+
+
+

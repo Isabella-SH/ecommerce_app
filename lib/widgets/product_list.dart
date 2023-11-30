e2f7e0cff650f126adb19341f30bd9319eec284d
Isabella <sorianoisabella28@gmail.com>
2023-11-29 21:35:55 -0500

    diseño de product_list.dart

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/models/product.dart';
 import 'package:flutter/material.dart';
 
 class ProductList extends StatefulWidget {
@@ -20,14 +21,54 @@ class _ProductListState extends State<ProductList> {
   @override
   Widget build(BuildContext context) {
 
-    return ListView.builder(
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), //crea 2 columnas
       itemCount: widget.products.length,
       itemBuilder: (context,index){
-        return ListTile(
-          title: Text(widget.products[index].name)
-        );
+        return ProductItem(product: widget.products[index]); //recibe cada indice de la lista
       }
     );
-
   }
 }
+
+class ProductItem extends StatefulWidget {
+  const ProductItem({super.key, required this.product});
+
+  //parametro
+  final Product product;
+
+  @override
+  State<ProductItem> createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: [
+        SizedBox(
+          height: 150,
+          child: Stack(
+            children: [
+              Image.network(widget.product.images[0],), //del arreglo de images escoge la pos 0
+              Positioned(
+                top:0,
+                right: 0,
+                child: IconButton(
+                  onPressed: (){},
+                  icon:const Icon(Icons.favorite), color: Colors.red,
+                )
+              ),
+            ]
+          )
+        ),
+        Text(widget.product.name)
+      ],
+    );
+  }
+}
+
+
+

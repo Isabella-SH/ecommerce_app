10afbad50de446adce55a4bffa85538662a9d052
Isabella <sorianoisabella28@gmail.com>
2023-12-05 15:59:43 -0500

    implementar accion de favorito en restaurant_list.dart

--- lib/widgets/restaurant_list.dart
+++ lib/widgets/restaurant_list.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/models/retaurant.dart';
+import 'package:ecommerce_app/repositories/restaurant_favorites_repository.dart';
 import 'package:ecommerce_app/services/restaurant_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -57,13 +58,38 @@ class RestaurantItem extends StatefulWidget {
 
 class _RestaurantItemState extends State<RestaurantItem> {
 
+  //atributos
+  bool _favorite=false;
+  RestaurantFavoritesRepository? _restaurantFavoritesRepository;
+
+
+  initialize()async{
+    //_favorite toma el valor que da el metodo del repositorie al pasarle cada entidad
+    _favorite=await _restaurantFavoritesRepository?.isFavorite(widget.restaurant)??false;
+    if (mounted){ //si ya se cargo toda la informacion
+      setState(() {
+        _favorite=_favorite;
+      });
+    }
+  }
+
+  @override
+  void initState(){
+    _restaurantFavoritesRepository=RestaurantFavoritesRepository();
+    initialize();
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
     //creo la imagen
     final image=Image.network(widget.restaurant.poster!);   // '!' porque el atributo esta como '?'
 
-    final my_con= const Icon(
+    final my_con= _favorite? const Icon(
+      Icons.delete,color:Colors.deepOrangeAccent,
+    ):const Icon(
       Icons.favorite,color:Colors.deepOrangeAccent,
     );
 
@@ -77,7 +103,13 @@ class _RestaurantItemState extends State<RestaurantItem> {
           icon: my_con,
           //dentro del click
           onPressed: (){
-
+            //_favorite cambia
+            setState(() {
+              _favorite=!_favorite;
+            });
+            //se agrega o elimina del repository
+            //si es true lo insert, sino lo elimina
+            _favorite?_restaurantFavoritesRepository?.insert(widget.restaurant):_restaurantFavoritesRepository?.delete(widget.restaurant);
           },
         ),
       ),

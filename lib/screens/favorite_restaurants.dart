7b266057afbd349f8eb6e8b99a3e85ab34c57200
Isabella <sorianoisabella28@gmail.com>
2023-12-05 16:10:00 -0500

    implementar vista de favorite_restaurants

--- lib/screens/favorite_restaurants.dart
+++ lib/screens/favorite_restaurants.dart
@@ -1,5 +1,9 @@
 
+import 'package:ecommerce_app/models/retaurant.dart';
+import 'package:ecommerce_app/repositories/restaurant_favorites_repository.dart';
+import 'package:ecommerce_app/widgets/restaurant_list.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FavoriteRestaurants extends StatefulWidget {
   const FavoriteRestaurants({super.key});
@@ -9,8 +13,38 @@ class FavoriteRestaurants extends StatefulWidget {
 }
 
 class _FavoriteRestaurantsState extends State<FavoriteRestaurants> {
+
+
+  RestaurantFavoritesRepository? _restaurantFavoritesRepository;
+  List<Restaurant>? _restaurants;
+
+  initialize() async{ // "?? []" -> si es nulo, devuelve un arreglo vacio
+    _restaurants=await _restaurantFavoritesRepository?.getAll()??[];
+    setState((){
+      _restaurants=_restaurants;
+    });
+  }
+
+  @override
+  void initState(){
+    _restaurantFavoritesRepository=RestaurantFavoritesRepository();
+    initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("restaurantes fav");
+
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: _restaurants?.length ?? 0,
+            itemBuilder: ((context, index) {
+              final restaurant = _restaurants![index];
+              return RestaurantItem(restaurant: restaurant);
+            }
+            )
+        )
+    );
   }
+
 }
